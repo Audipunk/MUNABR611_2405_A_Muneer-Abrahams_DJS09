2b7b83569e9b48ee61a3c2c4381c4bcf0caaae31
Muneer Abrahams <audipunkwp@gmail.com>
2025-03-12 23:25:07 +0200

    src/components/App/index.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,14 @@
 import { StorybookConfig } from '@storybook/react-native';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { } from './components/App';
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+  <App />
+  </StrictMode>
+);
 
 const main: StorybookConfig = {
   stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
