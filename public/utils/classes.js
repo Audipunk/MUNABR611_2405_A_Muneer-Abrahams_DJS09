45ce56ba0e3b6ca9e38d7fb6faade1296bc72f5f
Muneer Abrahams <audipunkwp@gmail.com>
2025-03-13 00:46:25 +0200

    src/components/App/style.css

--- public/utils/classes.js
+++
@@ -1,8 +0,0 @@
-class MainProperty {
-    constructor(src, title, reviews) {
-        this.src = src;
-        this.title = title;
-        this.reviews = reviews;
-    }
-}
-export default MainProperty;
